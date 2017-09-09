4c756850a18b6b6b332678bce7c0d5b198a40fbf
wojtczat <wojtczat@mcmaster.ca>
2017-09-09 03:26:13 -0400

    added user location

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,24 +1,56 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet, Text } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
-import { MapView } from 'expo'
+import { MapView, Permissions, Location } from 'expo'
 
 export default class LinksScreen extends React.Component {
+  state = {
+    isLoading: true,
+    location: null,
+  };
+
   static navigationOptions = {
     title: 'Map',
   };
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    console.log("Location:");
+    console.log(location);
+    this.setState({ location, isLoading: false });
+  };
+
+  componentWillMount = () => {
+    this._getLocationAsync();
+  };
+
+
   render() {
-    return (
-      <MapView
-        style={{ flex: 1 }}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      />
-    );
+    if (this.state.isLoading) {
+      return (
+        <Text>Loading.......</Text>
+      )
+    } else {
+
+      return (
+        <MapView
+          style={{ flex: 1 }}
+          initialRegion={{
+            latitude: this.state.location.coords.latitude,
+            longitude: this.state.location.coords.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      );
+    }
   }
 }
 
