64b7db42e5ae03148cc02e2f6560401dc0dde2c6
inksplatt <30760803+sophiatao@users.noreply.github.com>
2017-09-09 21:20:20 -0400

    Install react-native-location

--- screens/AnimalScreen.js
+++ screens/AnimalScreen.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import {Button} from 'react-native';
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCXbOsdGeX0RwN-rggwomI5Ps7nSDXXioQ",
+  authDomain: "fir-test-c4a21.firebaseapp.com",
+  databaseURL: "fir-test-c4a21.firebaseio.com",
+  storageBucket: "fir-test-c4a21.appspot.com"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+function setLocation(species, lat, long) {
+      firebase.database().ref('test4/' + species).push({
+        latitude: lat,
+        longitude: long
+      });
+}
 
 export default class AnimalScreen extends React.Component {
     static navigationOptions = ({ navigation }) => ({
         title: `${navigation.state.params.species}`,
   });
+
+      state = {
+    location: null,
+  };
+
     render() {
          const { navigate } = this.props.navigation;
-        return <Button title={'Take another picture'} onPress={() => navigate('Cam')}/>
+        return <Button title={'Take another picture'} onPress={() => navigate('Cam')}></Button>
 
     }
 }
